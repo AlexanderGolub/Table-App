98afeab273981e5b8ffce9a4016e3eecda037fba
AlexanderGolub <alaraks95@gmail.com>
2018-01-03 21:41:21 +0300

    Rename component App -> Table

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import App from './App';
+import Table from './Table';
 
 class Root extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return(
       <Provider store={this.props.store}>
         <Router>
           <Route path="/">
-            <Route path="/users" component={App} />
+            <Route path="/users" component={Table} />
           </Route>
         </Router>
       </Provider>
