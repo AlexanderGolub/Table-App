d2abab2f9cc22461b3f6daff45eb9b7e222b51b7
MINSK\Aliaksandr_Holub <aliaksandr_holub@epam.com>
2018-01-03 10:19:14 +0300

    Setup webpack and Babel

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+      inject: 'body'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
